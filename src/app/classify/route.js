f7a764025a4ca072aecb03043b06b8cd7dd317ec
salman44444 <salmanqurban36@gmail.com>
2024-08-11 09:09:30 +0500

    normalized the query to get more similar context

--- src/app/classify/route.js
+++ src/app/classify/route.js
@@ -22,16 +22,16 @@ export async function POST(req) {
     const extractor = await PipelineSingleton.getInstance();
 
     console.log('\n\n\n\n extractor made succesfull');
-    const query_embedded = await extractor(query);
+    const query_embedded = await extractor(query,{ pooling: 'mean', normalize: true });
 
     const vectorStore = await PineconeStore.fromExistingIndex(extractor, {
       pineconeIndex: index,
     });
     console.log('\n\n\n\n\n Vector Store defined successfully\n\nn\n\n\n');
   
-    const query_embedded_array = query_embedded.tolist()[0][1];
- //   console.log(query_embedded_array)
-
+    const query_embedded_array = query_embedded.tolist()[0];
+     console.log(query_embedded_array)
+    console.log('size is ',query_embedded_array.length)
     const results = await vectorStore.similaritySearchVectorWithScore(
       query_embedded_array,
       5
@@ -50,7 +50,12 @@ export async function POST(req) {
     .join('\n\n');
   
     console.log("contenxt created\n\n\n",context)
-    const prompt = `You are an assistant for answering medical questions...\n\n${context}\n\nQuestion: ${query}`;
+    const prompt = `  "You are an assistant for answering medical questions regarding diseases ,symptoms and their treatment and causes."
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise. and make the answer in such a way that it feels like talking to a doctor and provide answers from the knowledge of context "
+    "\n\n"\n\n${context}\n\nQuestion: ${query}`;
     console.log("\n\n\n\nprompt generated")
     console.log(prompt)
     const response = await openai.chat.completions.create({
@@ -60,7 +65,8 @@ export async function POST(req) {
 
     //await extractor(query, { pooling: 'mean', normalize: true });
    const end = response.choices[0].message.content;
-    console.log(query_embedded);
+    console.log(query_embedded.tolist());
+    console.log("size of query :",query_embedded.tolist()[0].length)
     console.log(end);
     return NextResponse.json(end);
   } catch (error) {
